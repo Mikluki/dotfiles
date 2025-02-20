b370258d771a7065a35cd91f4dbd480a17371fc5
mklk <lukianov2077@gmail.com>
2025-02-20 23:27:36 +0300

    preview in new window by default

--- config/nvim/lua/mklk/plugins/markdown-preview.lua
+++ config/nvim/lua/mklk/plugins/markdown-preview.lua
@@ -1,5 +1,4 @@
-return -- install without yarn or npm
-{
+return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
@@ -11,5 +10,15 @@ return -- install without yarn or npm
 		-- Set keymap to toggle Markdown preview with <leader>mp
 		vim.api.nvim_set_keymap("n", "<leader>mp", ":MarkdownPreviewToggle<CR>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "<leader>mq", ":MarkdownPreviewStop<CR>", { noremap = true, silent = true })
+
+		-- Define OpenMarkdownPreview function
+		vim.cmd([[
+			function! OpenMarkdownPreview(url)
+				call jobstart(["brave-browser", "--new-window", a:url], {"detach": v:true})
+			endfunction
+		]])
+
+		-- Set the function for markdown-preview.nvim
+		vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
 	end,
 }

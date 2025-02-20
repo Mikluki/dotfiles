cffc3bc2b831f2497a9d15a76270eb1af5e21398
mklk <lukianov2077@gmail.com>
2025-02-20 23:27:51 +0300

    telescope notification history

--- config/nvim/lua/mklk/plugins/notify.lua
+++ config/nvim/lua/mklk/plugins/notify.lua
@@ -3,4 +3,9 @@ return {
 	opts = {
 		timeout = 1000,
 	},
+	config = function()
+		vim.keymap.set("n", "<leader>f.", function()
+			require("telescope").extensions.notify.notify()
+		end, { desc = "Open notification history with Telescope" })
+	end,
 }

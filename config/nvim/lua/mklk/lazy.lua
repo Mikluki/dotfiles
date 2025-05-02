4903ecd23974de431d31cd67266fd08380231a0c
mklk <lukianov2077@gmail.com>
2025-05-02 23:19:26 +0300

    feat: nvim: added crates-nvim plugin

--- config/nvim/lua/mklk/lazy.lua
+++ config/nvim/lua/mklk/lazy.lua
@@ -11,12 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "mklk.plugins" }, { import = "mklk.plugins.lsp" } }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-})
+require("lazy").setup(
+	{ { import = "mklk.plugins" }, { import = "mklk.plugins.lsp" }, { import = "mklk.plugins.rust" } },
+	{
+		checker = {
+			enabled = true,
+			notify = false,
+		},
+		change_detection = {
+			notify = false,
+		},
+	}
+)

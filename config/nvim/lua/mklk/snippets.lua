91a1cc0c28d58b08ee31b0bee4bac468bc07c8c4
mklk <lukianov2077@gmail.com>
2025-09-01 18:14:15 +0300

    wip: nvim: new snippets

--- config/nvim/lua/mklk/snippets.lua
+++ config/nvim/lua/mklk/snippets.lua
@@ -40,30 +40,40 @@ local snippets = {
 			t({ "", "" }),
 			t(":::"),
 		}),
-
-		-- You can add more markdown snippets here
 	},
 
 	tex = {
-		ls.add_snippets("tex", {
-			s("header_russian_XeLaTeX", {
-				t({
-					"% Change figure caption label to Russian",
-					"\\renewcommand{\\figurename}{Рисунок}",
-					"",
-					"% Direct page margin settings",
-					"\\usepackage[left=2.5cm, right=2.5cm, top=2.5cm, bottom=2.5cm]{geometry}",
-					"",
-					"% Direct line spacing setting",
-					"\\renewcommand{\\baselinestretch}{1.5}",
-					"\\normalsize",
-				}),
+		s("header_russian_XeLaTeX", {
+			t({
+				"% Change figure caption label to Russian",
+				"\\renewcommand{\\figurename}{Рисунок}",
+				"",
+				"% Direct page margin settings",
+				"\\usepackage[left=2.5cm, right=2.5cm, top=2.5cm, bottom=2.5cm]{geometry}",
+				"",
+				"% Direct line spacing setting",
+				"\\renewcommand{\\baselinestretch}{1.5}",
+				"\\normalsize",
+			}),
+		}),
+		s("header_minimal", {
+			t({
+				"\\documentclass{article}",
+				"\\usepackage{amsmath} % for align, gather, etc.",
+				"\\begin{document}",
+				"\\end{document}",
 			}),
 		}),
 	},
-	-- You can add snippets for other file types
-	-- lua = { ... },
-	-- python = { ... },
+
+	python = {
+		s("pyignore", {
+			t(" # pyright: ignore"),
+		}),
+	},
+
+	-- Define snippets properly for all filetypes
+	-- all = {	},
 }
 
 -- Register all snippets
@@ -71,31 +81,35 @@ for filetype, filetype_snippets in pairs(snippets) do
 	ls.add_snippets(filetype, filetype_snippets)
 end
 
+-- Function to insert pyright ignore
+vim.keymap.set("n", "<leader>li", function()
+	local line = vim.fn.getline(".")
+	if not line:find("# pyright: ignore", 1, true) then
+		vim.fn.setline(".", line .. "  # pyright: ignore")
+	end
+end, { noremap = true, silent = true, desc = "Append '# pyright: ignore' to end of current line" })
+
 -- Function to insert the figure snippet
 local function insert_figure_snippet()
-	-- Check if we're in a markdown file
 	local ft = vim.bo.filetype
 	if ft == "markdown" then
-		-- Expand the 'fig' snippet
 		ls.snip_expand(snippets.markdown[1])
 	else
-		print("figure snippet is only available in markdown files")
+		print("Snippet: figure is only for md files")
 	end
 end
 
 -- Function to insert the two figures snippet
 local function insert_two_figures_snippet()
-	-- Check if we're in a markdown file
 	local ft = vim.bo.filetype
 	if ft == "markdown" then
-		-- Expand the 'fig' snippet
 		ls.snip_expand(snippets.markdown[2])
 	else
-		print("Two figures snippet is only available in markdown files")
+		print("Snippet: two figures is only for md files")
 	end
 end
 
--- Define the keymaps directly in this file
+-- Define the keymaps
 vim.keymap.set("n", "<leader>mff", function()
 	insert_figure_snippet()
 end, { desc = "Insert markdown figure snippet", noremap = true, silent = true })
@@ -104,9 +118,37 @@ vim.keymap.set("n", "<leader>mf2", function()
 	insert_two_figures_snippet()
 end, { desc = "Insert two figures side by side", noremap = true, silent = true })
 
+-- ### EOL custom functions ###
+-- EOL do not write lets discuss
+local function insert_dono()
+	local text = "Do not write code yet, lets discuss."
+	local current_line = vim.fn.getline(".")
+	vim.fn.setline(".", current_line .. text)
+end
+
+vim.keymap.set({ "n" }, "<leader>ld", insert_dono, {
+	desc = "Insert 'Do not write code lets discuss'",
+	noremap = true,
+	silent = true,
+})
+
+-- EOL do not change code I will edit myself give short snippets of target code
+local function insert_write_piece()
+	local text = "Do not change code. Give full snippets of target areas. I will edit myself"
+	local current_line = vim.fn.getline(".")
+	vim.fn.setline(".", current_line .. text)
+end
+
+vim.keymap.set({ "n" }, "<leader>ls", insert_write_piece, {
+	desc = "Insert 'Do not write code lets discuss'",
+	noremap = true,
+	silent = true,
+})
+
 -- Return the module with the insert functions
 return {
 	snippets = snippets,
 	insert_figure_snippet = insert_figure_snippet,
 	insert_two_figures_snippet = insert_two_figures_snippet,
+	insert_dono = insert_dono,
 }

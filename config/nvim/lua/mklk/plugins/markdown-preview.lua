cc39bfe114b6ab6859bdabaf68728610f1fa6acd
mklk <lukianov2077@gmail.com>
2025-09-01 18:15:05 +0300

    wip: nvim: better preview for latex formulas

--- config/nvim/lua/mklk/plugins/markdown-preview.lua
+++ config/nvim/lua/mklk/plugins/markdown-preview.lua
@@ -1,16 +1,22 @@
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
+	ft = { "markdown", "tex" },
 	build = function()
 		vim.cmd([[Lazy load markdown-preview.nvim]])
 		vim.fn["mkdp#util#install"]()
 	end,
 	config = function()
+		-- Enable commands for all filetypes (this is the key!)
+		vim.g.mkdp_command_for_global = 1
+
+		-- Set recognized filetypes (this controls syntax highlighting)
+		vim.g.mkdp_filetypes = { "markdown", "tex" }
+
 		-- Set keymap to stop Markdown preview
 		vim.api.nvim_set_keymap("n", "<leader>mq", ":MarkdownPreviewStop<CR>", { noremap = true, silent = true })
 
-		-- Define OpenMarkdownPreviewApp function for app mode
+		-- Define browser functions
 		vim.cmd([[
             function! OpenMarkdownPreviewApp(url)
                 call jobstart(["brave-browser", "--app=" . a:url], {"detach": v:true})
@@ -42,7 +48,7 @@ return {
             command! MarkdownPreviewTab call StartMarkdownPreviewTab()
         ]])
 
-		-- Set keymaps for the two different modes
+		-- Set keymaps
 		vim.api.nvim_set_keymap("n", "<leader>mp", ":MarkdownPreviewApp<CR>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "<leader>mo", ":MarkdownPreviewTab<CR>", { noremap = true, silent = true })
 	end,
